f0044d96e8d995696933fd489ff41519e72d97ff
ksobon <sobon.konrad@gmail.com>
2020-04-30 13:34:09 -0500

    add export button

---
+++ Honeybee/WPF/Buttons/DropDownButton.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Media;
+// ReSharper disable UnusedMember.Global
+
+namespace Honeybee.Core.WPF.Buttons
+{
+    public class DropDownButton : ToggleButton
+    {
+        public DropDownButton()
+        {
+            // Bind the ToggleButton.IsChecked property to the drop-down's IsOpen property
+            var binding = new Binding("Menu.IsOpen") { Source = this };
+            SetBinding(IsCheckedProperty, binding);
+            DataContextChanged += (sender, args) =>
+            {
+                if (Menu != null)
+                    Menu.DataContext = DataContext;
+            };
+        }
+
+        public ContextMenu Menu
+        {
+            get { return (ContextMenu)GetValue(MenuProperty); }
+            set { SetValue(MenuProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuProperty = DependencyProperty.Register("Menu",
+            typeof(ContextMenu), typeof(DropDownButton), new UIPropertyMetadata(null, OnMenuChanged));
+
+        public ImageSource MainImage
+        {
+            get { return (ImageSource)GetValue(MainImageProperty); }
+            set { SetValue(MainImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MainImageProperty =
+            DependencyProperty.Register("MainImage", typeof(ImageSource), typeof(DropDownButton),
+                new UIPropertyMetadata(null));
+
+        public ImageSource HoverImage
+        {
+            get { return (ImageSource)GetValue(HoverImageProperty); }
+            set { SetValue(HoverImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoverImageProperty =
+            DependencyProperty.Register("HoverImage", typeof(ImageSource), typeof(DropDownButton),
+                new UIPropertyMetadata(null));
+
+        public ImageSource DisabledImage
+        {
+            get { return (ImageSource)GetValue(DisabledImageProperty); }
+            set { SetValue(DisabledImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisabledImageProperty =
+            DependencyProperty.Register("DisabledImage", typeof(ImageSource), typeof(DropDownButton),
+                new UIPropertyMetadata(null));
+
+        private static void OnMenuChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var dropDownButton = (DropDownButton)d;
+            var contextMenu = (ContextMenu)e.NewValue;
+            contextMenu.DataContext = dropDownButton.DataContext;
+        }
+
+        protected override void OnClick()
+        {
+            if (Menu == null) return;
+
+            Menu.PlacementTarget = this;
+            Menu.Placement = PlacementMode.Bottom;
+            Menu.IsOpen = true;
+        }
+    }
+}
